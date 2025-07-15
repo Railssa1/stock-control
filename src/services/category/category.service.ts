ace4fe167da2d4081181da6cb22d6b15bf84be81
Raissa <raissamalves@gmail.com>
2025-07-15 20:53:13 -0300

    adiciona serviço de categorias; implementa lógica para obter todas as categorias da API

---
+++ src/services/category/category.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { environments } from 'src/environments/environments';
+import { CategoryResponse } from 'src/models/interfaces/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private urlApi = environments.apiUrl;
+  private JWT_TOKEN = this.cookie.get("JWT_TOKEN");
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.JWT_TOKEN}`
+    })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private cookie: CookieService
+  ) { }
+
+  getAllCategories(): Observable<Array<CategoryResponse>> {
+    return this.http.get<Array<CategoryResponse>>(`${this.urlApi}/categories`, this.httpOptions);
+  }
+}
